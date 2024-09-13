ba53d698477e0c84895f85d8f3b9ec5b2cef03d8
Michele <michele@michelepip.xyz>
2024-09-13 14:45:02 +0930

    Finalise abstract commands. Ready for merge.

--- src/main/java/xyz/nameredacted/disciplinex/cmd/moderation/WarnCommand.java
+++ src/main/java/xyz/nameredacted/disciplinex/cmd/moderation/WarnCommand.java
@@ -1,17 +1,18 @@
 package xyz.nameredacted.disciplinex.cmd.moderation;
 
+import net.kyori.adventure.text.TextComponent;
 import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.ApiStatus;
 import org.jetbrains.annotations.NotNull;
 
 import static xyz.nameredacted.disciplinex.staticaccess.StaticAccess.COMMAND_FAILED;
 import static xyz.nameredacted.disciplinex.staticaccess.StaticAccess.PLAYERS_ONLY_MSG;
 
-public class WarnCommand implements CommandExecutor {
-    @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd,
+public class WarnCommand extends xyz.nameredacted.disciplinex.cmd.Command {
+    @ApiStatus.Obsolete
+    public boolean obseleteOnCommand(@NotNull CommandSender sender, @NotNull Command cmd,
                              @NotNull String s, @NotNull String[] args) {
         if (!(sender instanceof Player)) {
             sender.sendMessage(PLAYERS_ONLY_MSG);
@@ -22,5 +23,25 @@ public class WarnCommand implements CommandExecutor {
 
 
         return COMMAND_FAILED;
+    }
+
+    @Override
+    protected boolean hasPermission(CommandSender sender) {
+        return false;
+    }
+
+    @Override
+    protected boolean areArgsValid(String[] args) {
+        return false;
+    }
+
+    @Override
+    protected boolean execute(Player player, String[] args) {
+        return false;
+    }
+
+    @Override
+    protected TextComponent getUsageMessage() {
+        return null;
     }
 }
