ed5c8f1e6aa9f6a306fcc689538cff6ef72d056a
Michele <michele@michelepip.xyz>
2024-09-20 10:19:27 +0930

    setup punishment inventory

--- src/main/java/xyz/nameredacted/disciplinex/cmd/moderation/punish/PunishCommand.java
+++ src/main/java/xyz/nameredacted/disciplinex/cmd/moderation/punish/PunishCommand.java
@@ -8,9 +8,15 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import xyz.nameredacted.disciplinex.DisciplineX;
+import xyz.nameredacted.disciplinex.api.Punishment;
 import xyz.nameredacted.disciplinex.cmd.Command;
+
+import java.util.List;
 
 import static xyz.nameredacted.disciplinex.staticaccess.StaticAccess.COMMAND_SUCCESS;
 import static xyz.nameredacted.disciplinex.staticaccess.StaticAccess.PLUGIN_PREFIX;
@@ -19,13 +25,17 @@ import static xyz.nameredacted.disciplinex.staticaccess.StaticAccess.PLUGIN_PREF
  * This is an implementation of every punishment command, and abstracts it into
  * an easy-to-use Graphical User Interface. It constructs a GUI based on what arguments the
  * staff member inputs:
- *
+ * <p>
  * No arguments: Display a GUI with every player.
  * One argument (Player to punish has been added): Skip the first step, and display the punishments the player can execute.
- *
+ * <p>
  * The player executing this command MUST have permissions to execute this command, and at leas
  */
 public class PunishCommand extends Command {
+
+    private final List<Component> noPermissionOrCannotUse = List.of(
+            Component.text("Either you do not have the required permissions to execute this punishment, or the player already has this punishment.").color(NamedTextColor.RED)
+    );
 
     @Override
     protected boolean hasPermission(CommandSender sender) {
@@ -70,7 +80,53 @@ public class PunishCommand extends Command {
     private Inventory createPunishmentSelectionGui(Player p, Player target) {
         Inventory inv = p.getServer().createInventory(null, 36, "Punish " + target.getName());
         // Add items to the inventory
-        inv.setItem(10, getSkullItem(target)); // Player's head
+        inv.setItem(4, getSkullItem(target)); // Player's head
+
+        // Get every active punishment for target player.
+        List<Punishment> activePunishments = DisciplineX.getInstance().getDb().getActivePunishments(target.getUniqueId());
+        // Check if the player has an active punishment for each type.
+        boolean isMuted = false;
+        boolean isBanned = false;
+
+        for (final Punishment activePunishment : activePunishments) {
+            switch (activePunishment.getPunishmentType()) {
+                case MUTE:
+                    isMuted = true;
+                    break;
+                case BAN:
+                    isBanned = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Create each item corresponding to warn, kick, ban, mute. If the player is already muted/banned, add a barrier block. If the executor does not have permissions to one of the punishments, add a barrier block.
+        // Mute
+        if (!isMuted && p.hasPermission("disciplinex.moderation.mute")) {
+            addItemToInventory(inv, createItemStack(Material.FEATHER, "Mute", null), 10);
+        } else {
+            addItemToInventory(inv, createItemStack(Material.BARRIER, "Mute", noPermissionOrCannotUse), 10);
+        }
+        // Repeat for other punishments
+        // Ban
+        if (!isBanned && p.hasPermission("disciplinex.moderation.ban")) {
+            addItemToInventory(inv, createItemStack(Material.IRON_SWORD, "Ban", null), 12);
+        } else {
+            addItemToInventory(inv, createItemStack(Material.BARRIER, "Ban", noPermissionOrCannotUse), 12);
+        }
+        // Kick
+        if (p.hasPermission("disciplinex.moderation.kick")) {
+            addItemToInventory(inv, createItemStack(Material.DIAMOND_SWORD, "Kick", null), 14);
+        } else {
+            addItemToInventory(inv, createItemStack(Material.BARRIER, "Kick", noPermissionOrCannotUse), 14);
+        }
+        // Warn
+        if (p.hasPermission("disciplinex.moderation.warn")) {
+            addItemToInventory(inv, createItemStack(Material.REDSTONE, "Warn", null), 16);
+        } else {
+            addItemToInventory(inv, createItemStack(Material.BARRIER, "Warn", noPermissionOrCannotUse), 16);
+        }
 
         return inv;
     }
@@ -78,15 +134,46 @@ public class PunishCommand extends Command {
     /**
      * This function generates an ItemStack (an item) with some
      * player's head (given by target)
+     *
      * @param target The player whose head we want to get
      * @return An ItemStack with the player's head
      */
     private ItemStack getSkullItem(@NotNull Player target) {
         ItemStack item = new ItemStack(Material.PLAYER_HEAD); // Create item
-        SkullMeta skullMeta = (SkullMeta)item.getItemMeta(); // Get item metadata
+        SkullMeta skullMeta = (SkullMeta) item.getItemMeta(); // Get item metadata
         skullMeta.setOwningPlayer(target); // Set skull to player's head.
         item.setItemMeta(skullMeta); // Set the item's metadata to the skull metadata
 
         return item;
+    }
+
+    /**
+     * This function generates an itemstack given some parameters.
+     * @param material The material of the item
+     * @param name The name of the item
+     * @param lore The lore of the item
+     * @return An ItemStack with the given parameters
+     */
+    private ItemStack createItemStack(@NotNull Material material, @Nullable String name, @Nullable List<Component> lore) {
+        ItemStack item = new ItemStack(material);
+        ItemMeta meta = item.getItemMeta();
+        assert meta != null;
+        if (name == null)
+            meta.displayName();
+        else
+            meta.displayName(Component.text(name).color(NamedTextColor.WHITE));
+        if (lore != null)
+            meta.lore(lore);
+        item.setItemMeta(meta);
+
+        return item;
+    }
+
+    /**
+     * This function adds an item to an inventory.
+     * @param item to add
+     */
+    private void addItemToInventory(@NotNull Inventory inv, @NotNull ItemStack item, int index) {
+        inv.setItem(index, item);
     }
 }
